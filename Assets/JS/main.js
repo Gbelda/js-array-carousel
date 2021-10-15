8c47bbaccf3c2e5e17ff8e85f4c17199f5114961
gbelda <gbelda56@gmail.com>
2021-10-15 16:01:30 +0200

    Applied button down img cycle

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,10 +1,10 @@
 //ARRAY procurati
 const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg'
 ];
 
 const title = [
@@ -22,4 +22,25 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+//Elementi da utilizzare
+const btnDown = document.getElementById("select_down")
+const btnUp = document.getElementById("select_up")
+
+//Stabilire il variabile per hero img
+
+var i = 1;
+
+btnDown.addEventListener('click', function () {
+    document.hero_img.src = items[i]
+
+    if (i < items.length - 1) {
+        i++
+
+    } else {
+        i = 0;
+    }
+
+
+})
 
